5335b7affb6726e9062144de7731a0ec9d00569a
SHUCHISMITA MALLICK <86481925+Shuchismita2000@users.noreply.github.com>
2022-02-05 20:34:56 +0530

    Update limit.py

--- apps/limit.py
+++ apps/limit.py
@@ -8,12 +8,13 @@ def app():
     with st.container():
         st.subheader("Limitation & Future Goal")
         st.markdown("""
-            1. We have no proper structure of data which can we get from hospitals even every type of user's need.
-             So, for now , we assume and create the data
-            2. We have to create a prediction model which can predict weights of attribute. But ,till we cannot able to 
-            create that model ,so we assume the weights
-            3. We have to generalize the functions of usability and associativity, for now ,for limited no of attributes 
-            we do every line of code manually
-            4. Setting the threshold value is not the proper way to predict sensitivity, we have to create a function or 
-            range which will applicable every unknown attributes
+        
+            1. We have no proper structure of data which can we get from hospitals even every type of user's need.So, for now , we assume and create the data
+             
+            2. We have to create a prediction model which can predict weights of attribute. But ,till we cannot able to create that model ,so we assume the weights
+            
+            3. We have to generalize the functions of usability and associativity, for now ,for limited no of attributes we do every line of code manually
+            
+            4. Setting the threshold value is not the proper way to predict sensitivity, we have to create a function or range which will applicable every unknown attributes
+            
             """)
